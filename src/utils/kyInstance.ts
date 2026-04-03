56538e5b3b1e753a48bef36710050fb12c580309
ATOHOUN Andy <elomanfred@gmail.com>
2026-04-03 21:55:28 +0100

    Removed the commit from the main branch

--- src/utils/kyInstance.ts
+++ src/utils/kyInstance.ts
@@ -1,3 +1,20 @@
 import ky from "ky";
 
 const url = import.meta.env.VITE_API_URL;
+
+const kyInstance = ky.create({
+  prefixUrl: url,
+});
+
+export const kyPrivateInstance = ky.extend({
+  hooks: {
+    beforeRequest: [
+      (request) => {
+        const token = "MY_TOKEN";
+        request.headers.set("Authorization", `Bearer ${token}`);
+      },
+    ],
+  },
+});
+
+export default kyInstance;
