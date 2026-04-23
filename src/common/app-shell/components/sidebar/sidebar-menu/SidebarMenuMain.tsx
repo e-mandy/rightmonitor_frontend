e5472a69464a43a1dcc1fcf35fa56d55502604fd
ATOHOUN Andy <elomanfred@gmail.com>
2026-04-23 03:17:51 +0000

    feat(sidebar): Added right sidebar items for each page

--- src/common/app-shell/components/sidebar/sidebar-menu/SidebarMenuMain.tsx
+++ src/common/app-shell/components/sidebar/sidebar-menu/SidebarMenuMain.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/jsx-no-target-blank */
 // import { useIntl } from "react-intl";
 import { SidebarMenuItemWithSub } from "./SidebarMenuItemWithSub";
 import { SidebarMenuItem } from "./SidebarMenuItem";
@@ -9,26 +8,37 @@ const SidebarMenuMain = () => {
   return (
     <>
       <SidebarMenuItem
-        to="dashboard"
+        to="/dashboard"
         title="Dashboard"
         fontIcon="bi-archive"
         icon="ranking"
       />
 
-      <SidebarMenuItemWithSub
-        to="/test"
-        title="Menu"
-        icon="notepad-bookmark"
-      >
+      <SidebarMenuItemWithSub to="" title="Overview" icon="notepad-bookmark">
+        <SidebarMenuItem to="/journey" title="Journey Feed" hasBullet={true} />
+
+        <SidebarMenuItem to="/onboarding" title="Onboarding" hasBullet={true} />
+      </SidebarMenuItemWithSub>
+
+      <SidebarMenuItemWithSub to="" title="Companies" icon="notepad-bookmark">
         <SidebarMenuItem
-          to="subtest"
-          title="Sub Menu"
+          to="/dashboard"
+          title="All Companies"
           hasBullet={true}
         />
 
-        <SidebarMenuItem to="subtest1" title="Sub Menu 1" hasBullet={true} />
+        <SidebarMenuItem to="/at-risk" title="At Risk" hasBullet={true} />
       </SidebarMenuItemWithSub>
 
+      <SidebarMenuItemWithSub to="" title="Report" icon="notepad-bookmark">
+        <SidebarMenuItem
+          to="/weekly-report"
+          title="Weekly Report"
+          hasBullet={true}
+        />
+
+        <SidebarMenuItem to="/analytics" title="Analytics" hasBullet={true} />
+      </SidebarMenuItemWithSub>
     </>
   );
 };
