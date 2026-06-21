1b3ac5c528b892bbe19745ea21cfbff7a255634f
ATOHOUN Andy <elomanfred@gmail.com>
2026-06-22 00:06:19 +0100

    feat: Updated the analytics section.

--- src/components/analytics/CustomerSuccessHealth.tsx
+++ src/components/analytics/CustomerSuccessHealth.tsx
@@ -1,13 +1,12 @@
 import GlobalCustomerKPI from "./customer_success/GlobalCustomerKPI";
-import PipelineExpansion from "./customer_success/PipelineExpansion";
 
 const CustomerSuccessHealth = () => {
   return (
     <div>
       <GlobalCustomerKPI />
-      <div className="d-flex">
+      {/* <div className="d-flex">
         <PipelineExpansion />
-      </div>
+      </div> */}
     </div>
   );
 };
